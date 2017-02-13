ce2a0642b626161ac42f9315eb17d6e4638fcd8e
Ines <agnes19gomez@gmail.com>
2017-02-13 15:55:42 +0100

    Play

---
+++ app/src/main/java/sdm/labs/sevilpon/millonario/Scores.java
@@ -0,0 +1,13 @@
+package sdm.labs.sevilpon.millonario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Scores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores);
+    }
+}
