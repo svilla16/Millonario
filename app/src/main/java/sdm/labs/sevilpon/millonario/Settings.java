16bc24338c8b874160bd9d9661273238be0fac5e
Ines <agnes19gomez@gmail.com>
2017-03-01 16:38:32 +0100

    scores

--- app/src/main/java/sdm/labs/sevilpon/millonario/Settings.java
+++ app/src/main/java/sdm/labs/sevilpon/millonario/Settings.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Settings extends AppCompatActivity {
 
@@ -11,5 +13,9 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Spinner spinnerCountShoes = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> spinnerCountShoesArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.ayuda));
+        spinnerCountShoes.setAdapter(spinnerCountShoesArrayAdapter);
+
     }
 }
