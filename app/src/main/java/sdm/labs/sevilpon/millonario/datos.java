dc6591e71cabc848a6050562da575bc07afe6674
Agnes20 <agnes19gomez@gmail.com>
2017-03-06 15:31:43 +0100

    Add files via upload

---
+++ app/src/main/java/sdm/labs/sevilpon/millonario/datos.java
@@ -0,0 +1,23 @@
+package sdm.labs.sevilpon.millonario;
+
+public class datos {
+
+        String nombre;
+
+        int puntuacion;
+
+
+        public String getnombres() {
+            return nombre;
+        }
+
+        public void setnombres(String nombre) {this.nombre = nombre;}
+
+        public int getpuntuacions() {
+            return puntuacion;
+        }
+
+        public void setpuntuacions(int puntuacion) {
+            this.puntuacion = puntuacion;
+        }
+}
